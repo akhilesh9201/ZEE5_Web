7d76cfd2f79a7621af89346d625692be09b0b166
akhilesh9201 <60286457+akhilesh9201@users.noreply.github.com>
2020-09-16 14:41:37 +0530

    Created pages

---
+++ pages/CustomActions.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-content>
+      It Worked! Yes It did Great
+      <v-container>
+        <v-row justify="center">
+        
+        </v-row>
+        <CustomActions/>
+      </v-container>
+    </v-content>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
