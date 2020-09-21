1168f287f137556a083d694687a29e631baeccfa
akhilesh9201 <60286457+akhilesh9201@users.noreply.github.com>
2020-09-21 15:48:10 +0530

    Update FormBasic.vue

--- pages/FormBasic.vue
+++ pages/FormBasic.vue
@@ -4,7 +4,7 @@
       It Worked! Yes It did Great
       <v-container>
         <v-row justify="center">
-          <h1>Stay home or suffer</h1>
+          <h1>User Details</h1>
         </v-row>
           <FormBasic/>
       </v-container>
