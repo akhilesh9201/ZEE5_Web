1045f0a7627ce690bbd9cf0e513790eb748220bf
akhilesh9201 <60286457+akhilesh9201@users.noreply.github.com>
2020-10-24 21:41:02 +0530

    footer changes

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,33 +1,136 @@
 <template>
   <v-footer
-    dark
-  >
-    <v-card   
-      class="rgb(29, 0, 30)"
+    class="mt-lg-16 mt-md-16 mt-sm-16 mt-16" 
+    color="black"
     >
-     
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4 white--text"
-          icon
-        >
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
+
+    <v-container>
+     <v-row>
+        <v-col 
+        class="pt-md-8"
+        cols="12"
+        sm="2"
+        md="2"
+        lg="2"
+        xs="2"
+        > 
+        <h4>About Us</h4>
+        <v-spacer></v-spacer>
+        <br>
+        <v-divider></v-divider><br>
+        <h4>Help Center</h4>
+        <br>
+        <v-divider></v-divider><br>
+        <h4>Privacy Policy</h4>
+        <br>
+        <v-divider></v-divider><br>
+        <h4>Parental Control</h4>
+        <br>
+        <v-divider></v-divider><br>
+        <h4>Terms Of Use</h4>
+      </v-col> 
       
+      <v-col
+      cols="12"
+      sm="2"
+      md="2"
+      lg="2"
+      xs="2"
+      >
+      <h4>ZEE5 Original</h4>
+      <br>
+      <font size="2">State of Seige: 26/11
+      <br>
+      XXX <br>
+      Mentalhood<br>
+      Rangbaaz<br>
+      <br></font>
+       <v-divider></v-divider>
+      <br>
+      <h4>Premium Zee5 Originals</h4>
+      <br>
+      <font size="2">Mafia
+      <br>Reject
+      <br>Lalbazar
+      <br>Rangbaaz</font>
+      </v-col>
 
-    <v-card-text>
-    <v-col cols="10"> 
-      About Us
-      Hello
-    </v-col>
-    </v-card-text>
-
+      <v-col
+      cols="12"
+      md="2"
+      lg="2"
+      sm="2"
+      xs="2"
+      >
+      <h4>Club Original</h4><br>
+      <font size="2">Fitrat<br>
+      Jamai 2.0<br>
+      Karrle Tu Fir Mohabbat<br>
+      Daarish<br><br></font>
       <v-divider></v-divider>
+      <br>  
+      <h4>Premium Zee5 Originals</h4>
+      <br>
+      <font size="2">Mafia
+      <br>Reject
+      <br>Lalbazar
+      <br>Rangbaaz</font>
+      </v-col>
+      
+      <v-col
+       cols="12"
+      md="2"
+      lg="2"
+      sm="2"
+      xs="2"
+      >
+      <h4>ZEE5 Premium Movies</h4>
+      <br>
+      <font size="2">Ateet<br>
+      Commando<br>
+      Simbba<br>
+      Uri:The Surgical Strike<br></font>
+      <br><v-divider></v-divider><br>
+      <h3>Download Apps</h3>
+      <br><br>
+      <h3>Connect With Us</h3>
+      </v-col>
 
-     
-    </v-card>
+      <v-col
+      cols="12"
+      md="2"
+      lg="2"
+      sm="2"
+      xs="2"
+      >
+      <h4>TV Shows</h4>
+      <br>
+      <font size="2">Kumkum Bhagya<br>
+      Kundali Bhagya<br>
+      Kehna ko Humsafar Hai<br>
+      Tuzhse hai Raabta<br></font><br><br>
+     <v-img src="GooglePlay.jpg"> </v-img>
+     <v-img src="YTLogo.png"> </v-img>
+      </v-col>
+        
+
+
+      <v-col
+      cols="12"
+      md="2"
+      lg="2"
+      sm="2"
+      xs="2"
+      >
+      <h4>News</h4>
+      <br>
+      <font size="2">Corona Outbreak<br>
+      Today's Headline<br>
+      Trending Topics<br>
+      Entertainment News<br></font>
+      </v-col>
+
+     </v-row>     
+    </v-container>
   </v-footer>
 </template>
